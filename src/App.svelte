e8f9a1490e6a097cddff092c4e1be83bd2e3048a
Adwithya Magow <avmagow@gmail.com>
2024-02-05 00:21:52 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,116 @@
+<script>
+	import { tweened } from "svelte/motion";
+	import { onDestroy } from 'svelte';
+
+	let isValentine = false;
+	let fontSize = "1em";
+
+	let handleNo = () => {
+		fontSize = parseFloat(fontSize) + 1 + "em"; // Increment fontSize by 1
+	};
+
+	let handleYes = () => {
+		isValentine = true;
+	};
+
+	let hearts = new Array(100).fill().map((_, i) => {
+		return {
+			id: i,
+			x: Math.random() * 100,
+			y: Math.random() * 100,
+		};
+	});
+</script>
+
+<main>
+	<div class="container">
+		<div class="content">
+			{#if isValentine}
+				<h1>Yayyyyyyy! Love you bb! 😍</h1>
+			{:else}
+				<div class="emoji">
+					<img src="valentine.gif" alt="Plankton getting a heart" />
+				</div>
+				<h1>Will you be my valentine?</h1>
+				<div class="btn-row">
+					<div class="btn-yes">
+						<button
+							style="font-size: {fontSize}"
+							on:click={handleYes}>Yes</button
+						>
+					</div>
+					<div class="btn-no">
+						<button on:click={handleNo}>No</button>
+					</div>
+				</div>
+			{/if}
+		</div>
+
+		{#each hearts as heart (heart.id)}
+			<div
+				class="heart"
+				style="left: {heart.x}%; top: {heart.y}%;"
+				key={heart.id}
+			>
+				❤️
+			</div>
+		{/each}
+	</div>
+</main>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		height: 100vh;
+	}
+
+	.content {
+		z-index: 100;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		position: relative;
+		background-color: white;
+	}
+
+	.btn-row {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.btn-yes button {
+		background-color: green;
+		color: white;
+		border-radius: 5px;
+	}
+
+	.btn-no button {
+		background-color: red;
+		color: white;
+		border-radius: 5px;
+	}
+
+	.emoji {
+		font-size: 5em;
+	}
+
+	.heart {
+		position: absolute;
+		animation: float 5s infinite;
+	}
+
+	@keyframes float {
+		0% {
+			transform: translateY(0);
+		}
+		50% {
+			transform: translateY(-20px);
+		}
+		100% {
+			transform: translateY(0);
+		}
+	}
+</style>
